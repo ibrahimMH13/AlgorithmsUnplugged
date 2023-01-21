66b2828b34a8cf66038412be1c34fcaa3ed8eeff
Ibrahim I. I. Musabeh <i.musabah92@gmail.com>
2023-01-21 16:01:11 +0800

    add binary search

--- Chapter1/BinarySearch.py
+++ Chapter1/BinarySearch.py
@@ -1,16 +1,20 @@
 a = range(5, 5000)
 
 
+# binary_search rely on sort list according values
+# so need ordered list
 def binary_search(a, value, left, right):
     while left <= right:
         middle = (left + right) // 2
-        print(middle)
         if a[middle] == value:
             return middle
         if a[middle] > value:
-            right = middle -1
+            right = middle - 1
         if a[middle] < value:
-            left = middle +1
+            left = middle + 1
     return "NOT FOUND"
 
-print(binary_search(a, 52, 0,len(a)))
+
+value = 1
+key = binary_search(a, value, 0, len(a))
+print(f"this value [{value}] hold in index no[{key}] in this range ")
